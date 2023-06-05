fb373ec0745b925438f7e60770a99e80d78e3022
Raj Rawariya <98628762+Raj100@users.noreply.github.com>
2023-06-06 00:47:05 +0530

    commit #4

--- lab9/areaperi.h
+++
@@ -1,6 +0,0 @@
-#define area_tri(x,y,z) 0.5*a
-#define pri_tri(a,b,c) a+b+c
-#define area_circle(r) 3.14*r*r
-#define peri_circle(r) 2*3.14*r
-#define area_square(s) s*s 
-#define peri_square(s) 4*s
